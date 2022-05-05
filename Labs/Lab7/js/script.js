73b65bd2c62e5ead659d23fed3d48158291fbe48
nlakhota <43013362+nlakhota@users.noreply.github.com>
2022-05-04 19:32:19 -0500

    Update script.js

--- Labs/Lab7/js/script.js
+++ Labs/Lab7/js/script.js
@@ -1,11 +1,10 @@
-function init() {
-    function alertFunction() {
+function init(){
+    function alertFunction(){
         var txt = document.getElementById("entryinput").value;
         alert("Nakibuddin Lakhota:" + txt);
         document.getElementById("textoutput").innerHTML = txt
     }
-    var button = document.getElementById('entrybutton');
-    button.addEventListener('click', alertFunction);
-
+    var button = document.getElementById("entryinput").value;
+    button.addEventListener('click', alertFunction)
 }
 window.addEventListener('load', init);
