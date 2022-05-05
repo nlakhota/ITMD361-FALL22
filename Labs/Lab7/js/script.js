6c079cfd139b177951aa8a10e29a79a7fbbb0972
nlakhota <43013362+nlakhota@users.noreply.github.com>
2022-05-04 19:22:07 -0500

    Update script.js

--- Labs/Lab7/js/script.js
+++ Labs/Lab7/js/script.js
@@ -1,11 +1,13 @@
-function init() {
-    function alertFunction() {
-        var txt = document.getElementById("entryinput").value;
-        alert("Dinora Saldivar:" + txt);
-        document.getElementById("textoutput").innerHTML = txt
+function init(){
+    //add your javascript between these two lines of code
+    function alertFunction(){
+        var text = document.getElementById("entryinput").value;
+        var text2 = document.getElementById("textoutput");
+        alert("Nakibuddin Nasiruddin Lakhota: " + text);
+        document.getElementById("textoutput").innerHTML = text
     }
     var button = document.getElementById('entrybutton');
     button.addEventListener('click', alertFunction);
-
 }
-window.addEventListener('load', init);
+
+    window.addEventListener('load', init);
